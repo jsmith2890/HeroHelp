87c47fdaf5760d06e031a2b1403896233d73500c
Jessica Smith <smith.j.m.13@gmail.com>
2018-08-21 19:26:48 -0500

    admin changes

---
+++ admin/client/components/admin.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import Heroes from './heroes'
+import Citizens from './citizens'
+import Incidents from './incidents'
+import CustomTable from './customtable'
+
+
+const Admin = ({data}) => {
+  return (
+    <div>
+      <h1>Heroes:</h1>
+      {data && data.heroes && <Heroes heroes={data.heroes}/>}
+      <h1>Incidents:</h1>
+      {data && data.incidents && <Incidents incidents={data.incidents}/>}
+      
+    </div>
+  )
+}
+
+const mapStateToProps = ({data}) => ({data})
+
+export default connect(mapStateToProps, null)(Admin)
+// <h1>Citizens:</h1>
+//       {data && data.citizens && <Citizens citizens={data.citizens}/>} 
